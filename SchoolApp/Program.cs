01db2112e4ab5a2f57638fad1bd99c85c7684dd4
petrosmeliras <petros.meliras@gmail.com>
2026-07-12 14:22:15 +0300

    Add project files.

---
+++ SchoolApp/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolApp.Data;
+
+namespace SchoolApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Scoped - per request, Transient - new instance every time, Singleton - one instance for the entire application
+            var connString = builder.Configuration.GetConnectionString("DevConnection");
+            builder.Services.AddDbContext<SchoolMvc9proContext>(options => options.UseSqlServer(connString));
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
