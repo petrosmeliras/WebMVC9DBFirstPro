27153cfc7c166646a80cf73d52c85609080ccdca
petrosmeliras <petros.meliras@gmail.com>
2026-07-12 17:17:11 +0300

    chore(repositories): add extension method for adding unit of work to IoC container

---
+++ SchoolApp/Repositories/RepositoriesDIEExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.Repositories
+{
+    public static class RepositoriesDIEExtensions
+    {
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
