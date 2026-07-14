6c6483f68422cae867da8da5a05bd808e8827cc8
petrosmeliras <petros.meliras@gmail.com>
2026-07-15 00:28:40 +0300

    chore(controllers): add controllers

--- SchoolApp/Controllers/HomeController.cs
+++ SchoolApp/Controllers/HomeController.cs
@@ -1,22 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SchoolApp.Models;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace SchoolApp.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            ClaimsPrincipal? principal = HttpContext.User;
+
+            if (!principal.Identity!.IsAuthenticated)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index", "User");
         }
 
+        [HttpGet]
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [AllowAnonymous]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- SchoolApp/Controllers/TeacherController.cs
+++ SchoolApp/Controllers/TeacherController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SchoolApp.Core;
 using SchoolApp.DTO;
 using SchoolApp.Services;
@@ -16,18 +17,21 @@ namespace SchoolApp.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "TEACHER")]
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult Signup()
         {
             return View();
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public async Task<IActionResult> Signup(TeacherSignupDTO teacherSignupDTO)
         {
             if (!ModelState.IsValid)
@@ -38,7 +42,7 @@ namespace SchoolApp.Controllers
             try
             {
                 await _applicationService.TeacherService.SignUpUserAsync(teacherSignupDTO);
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Login", "User");
             }
             catch (Exception ex)
             {
@@ -46,7 +50,9 @@ namespace SchoolApp.Controllers
                 ViewData["ErrorArray"] = ErrorArray;
                 return View(teacherSignupDTO);
             }
-
         }
+
+
+
     }
 }
