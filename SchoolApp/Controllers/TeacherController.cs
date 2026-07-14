d1ed9575f698088d66f48aae51265382935dc53c
petrosmeliras <petros.meliras@gmail.com>
2026-07-14 23:07:58 +0300

    chore(controllers): add teacher controller

---
+++ SchoolApp/Controllers/TeacherController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolApp.Core;
+using SchoolApp.DTO;
+using SchoolApp.Services;
+
+namespace SchoolApp.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly IApplicationService _applicationService;
+        public List<Error> ErrorArray { get; set; } = [];
+
+        public TeacherController(IApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(TeacherSignupDTO teacherSignupDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(teacherSignupDTO);
+            }
+
+            try
+            {
+                await _applicationService.TeacherService.SignUpUserAsync(teacherSignupDTO);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ErrorArray.Add(new Error("", ex.Message, ""));
+                ViewData["ErrorArray"] = ErrorArray;
+                return View(teacherSignupDTO);
+            }
+
+        }
+    }
+}
